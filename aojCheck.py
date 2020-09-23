6cdf07046a021b5f18a18617aec0f2e0f55f35fb
ishiharaTatsuki <jyah5uih2ksje@gmail.com>
2020-09-23 17:09:00 +0900

    commit

---
+++ aojCheck.py
@@ -0,0 +1,50 @@
+#
+# with open(r"C:\Users\tatuki\Desktop\atcoder_input.txt") as f:
+#     tree_data = [s.strip() for s in f.readlines()]
+#
+#
+# def input():
+#     try:
+#         return tree_data.pop(0)
+#     except:
+#         return ""
+#
+
+# S = tree_data.pop(0)
+# ---------------------------------------------------
+# ---------------------------------------------------
+# ---------------------------------------------------
+
+import sys
+
+sys.setrecursionlimit(10 ** 9 + 7)
+read = sys.stdin.buffer.read
+inp = sys.stdin.buffer.readline
+
+
+def inpS(): return inp().rstrip().decode()
+
+
+readlines = sys.stdin.readlines
+MOD = 10 ** 9 + 7
+INF = 1 << 60
+
+
+# ---------------------------------------------------
+# ---------------------------------------------------
+# ---------------------------------------------------
+
+
+
+
+def resolve():
+    print()
+
+
+
+if __name__ == '__main__':
+    import time
+
+    s = time.time()
+    resolve()
+    print("time: ", time.time() - s)
